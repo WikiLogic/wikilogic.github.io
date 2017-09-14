cccd30ede05b96e7ad7c09d9a7d68874db616cd8
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-09-13 22:47:09 -0400

    bat file creates the guestlist file

--- wikilogic-setup.bat
+++ wikilogic-setup.bat
@@ -6,6 +6,16 @@ git clone https://github.com/WikiLogic/wikilogic.github.io.git
 
 curl https://raw.githubusercontent.com/WikiLogic/wikilogic.github.io/master/docker-compose.dev.yml > docker-compose.dev.yml
 
+(
+    echo module.exports = {
+    echo     "people": [
+    echo        { "email": "you@email.com" },
+    echo        { "email": "test@test.com" },
+    echo        { "email": "test2@test.com" }
+    echo    ]
+    echo }
+)> "./api/guestlist.js"
+
 cd api
 call npm i
 pause
