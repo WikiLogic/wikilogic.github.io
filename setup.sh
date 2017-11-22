7fd2765f61936cac7f17b706de11995fafdafa84
ijmccallum <ijmccallum@hotmail.co.uk>
2017-11-21 21:50:36 -0500

    setup script update - working!

--- setup.sh
+++ setup.sh
@@ -1,14 +1,19 @@
+#This is where the magic happens
+mkdir -p "/var/www/wikilogic"
 
-mkdir -p /var/www/wikilogic
+#Jump in there and clone the code
 cd /var/www/wikilogic
-
 git clone https://github.com/WikiLogic/api.git
 git clone https://github.com/WikiLogic/react-app.git
 
-scp https://raw.githubusercontent.com/WikiLogic/wikilogic.github.io/master/docker-compose.dev.yml docker-compose.dev.yml
+#the api needs it's node_modules, so install npm and install the api packages
+cd /var/www/wikilogic/api
+apt-get update
+apt install npm
+npm install
 
-(cd api && npm install)
-
+#back in the main directory, set up docker and run it!
+cd /var/www/wikilogic
+wget -O /var/www/wikilogic/docker-compose.yml https://raw.githubusercontent.com/WikiLogic/wikilogic.github.io/master/docker-compose.yml
 sudo ufw allow 80
-
-docker-compose -f docker-compose.dev.yml up -d
+docker-compose up -d
