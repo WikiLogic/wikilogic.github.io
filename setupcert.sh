4b7578c5ffd66fb90816a05204a870fe37f68cb0
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-11-28 07:19:39 -0500

    cert setup tweak

--- setupcert.sh
+++ setupcert.sh
@@ -9,4 +9,4 @@ apt-get install python-certbot-nginx
 # We give it a directory on the server where it can put a file which will be visible at the site root, this is to confirm we own the domain
 # the react app dist folder is shared with the static docker container as a volume so that should be good.
 # Note that you will also need the domain to be pointing to this box, DNS changes can take a while so you're going to be without https for a bit
-certbot certonly --webroot -w /var/www/wikilogic/react-app/dist -d alpha.wikilogicfoundation.com
+certbot certonly --webroot -w /var/www/wikilogic/react-app/dist -d alpha.wikilogicfoundation.org
