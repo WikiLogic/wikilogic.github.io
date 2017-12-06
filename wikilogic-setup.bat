f4b3d652ca06e3f52562fd303e2e99e8ec5ee6aa
Iain J McCallum <ijmccallum@hotmail.co.uk>
2017-12-06 15:39:09 -0500

    2 less repos to install

--- wikilogic-setup.bat
+++ wikilogic-setup.bat
@@ -1,7 +1,5 @@
 git clone https://github.com/WikiLogic/react-app.git
 git clone https://github.com/WikiLogic/api.git
-git clone https://github.com/WikiLogic/Neo4JProcedures.git
-git clone https://github.com/WikiLogic/testing.git
 git clone https://github.com/WikiLogic/wikilogic.github.io.git
 
 curl https://raw.githubusercontent.com/WikiLogic/wikilogic.github.io/master/docker-compose.dev.yml > docker-compose.dev.yml
