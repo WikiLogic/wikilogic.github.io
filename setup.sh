9600e8b4251572dd973ae676f7a0c4059548ac03
Iain J McCallum <ijmccallum@hotmail.co.uk>
2018-03-12 20:06:28 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,27 +2,15 @@
 mkdir -p "/var/www/wikilogic"
 
 # Jump in there and clone the code
-cd /var/www/wikilogic
-git clone https://github.com/WikiLogic/api.git
-git clone https://github.com/WikiLogic/react-app.git
+cd /var/www
+git clone git clone --recurse-submodules -j8 https://github.com/WikiLogic/infrastructure.git wikilogic
 
-# The api needs it's node_modules, so...
-# cd /var/www/wikilogic/api
-# Install npm
-# apt-get update
-# apt install npm
-# Then use it to install the packages required by the api
-# npm install
-
-# Back in the main directory, set up docker and run it!
-# cd /var/www/wikilogic
-# Copy the dockerfile from github
-wget -O /var/www/wikilogic/docker-compose.yml https://raw.githubusercontent.com/WikiLogic/wikilogic.github.io/master/docker-compose.yml
 # Open the ports to the internet!
 sudo ufw allow 80
 sudo ufw allow 443
 
 # Turn it on. When you see a few parallel lines like "======>" growing, you're into the docker part.
+cd /var/www/wikilogic
 docker-compose up -d
 
 # TODO: certbot script
